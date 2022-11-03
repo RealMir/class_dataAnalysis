82515de585ec0880cde24d9e6aeef758763a8aba
realmir <jinyong0425@naver.com>
2022-11-03 00:44:32 +0000

    add loc, iloc conept

--- lesson/pandas/6_selection.py
+++ lesson/pandas/6_selection.py
@@ -22,3 +22,24 @@ print(df[:3]) # print(df[3]) is error because '3' is recognized by column not in
 changed_df = pd.read_excel("stock.xlsx",index_col="종목명") # change index
 print(changed_df[:"SK하이닉스"]) #same
 print(changed_df[:df["종목명"][3]]) #same
+
+
+
+""" loc select """
+# think like coordinates 
+# x is name of index, y is name of column
+print(df.loc[0])
+print(df.loc[0,"종목명"])
+print(df.loc[[0,2],["종목명","현재가"]])
+print(df.loc[0:2,"종목명":"현재가"])
+# when use slicing at loc, the back number is included ,unlike general method
+
+
+
+""" iloc select """
+# same like coordinates
+# x is number of index, y is number of column
+print(df.iloc[0])
+print(df.iloc[0,0])
+print(df.iloc[[0,2],[0,1]])
+print(df.iloc[0:2,0:1])
