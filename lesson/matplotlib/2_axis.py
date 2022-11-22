addb06af40dd4f601e6b0f06f5c7f522ad42d92c
realmir <jinyong0425@naver.com>
2022-11-22 11:18:52 +0000

    add rotaion,xlim,ylim

--- lesson/matplotlib/2_axis.py
+++ lesson/matplotlib/2_axis.py
@@ -17,6 +17,22 @@ plt.ylabel("Y축", color="#00FF00", loc="top")
 plt.show()
 
 plt.plot(x,y)
+plt.xlabel("X축", color="blue", loc="right") # loc = location
+plt.ylabel("Y축", color="#00FF00", loc="top", rotation=0)
+plt.show()
+
+plt.plot(x,y)
 plt.xticks([1,2,3])
 plt.yticks([])
+plt.show()
+
+z = ["1번","2번","3번"]
+plt.plot(x,y)
+plt.xticks(x,z)
+plt.yticks(rotation=45)
+plt.show()
+
+plt.plot(x,y)
+plt.xlim(0,5) # setting x range to 0~5
+plt.ylim(-5,5) # setting y range to -5~5
 plt.show()
